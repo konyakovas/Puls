f3a0910cf3def1ae45464dcd7946cdde739506cf
Anton <konyakov_as@mail.ru>
2021-01-20 20:03:00 +0300

    wow.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
   $('.carousel__inner').slick({
       speed: 1200,
       adaptiveHeight: true,
-      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/left.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/right.png"></button>',
       responsive: [
           {
               breakpoint: 992,
@@ -116,22 +116,7 @@ $(document).ready(function(){
             return false;
     });
 
-
-    const wow = new WOW(
-        {
-          boxClass:     'wow',      // animated element css class (default is wow)
-          animateClass: 'animated', // animation css class (default is animated)
-          offset:       0,          // distance to the element when triggering the animation (default is 0)
-          mobile:       true,       // trigger animations on mobile devices (default is true)
-          live:         true,       // act on asynchronously loaded content (default is true)
-          callback:     function(box) {
-            // the callback is fired every time an animation is started
-            // the argument that is passed in is the DOM node being animated
-          },
-          scrollContainer: null // optional scroll container selector, otherwise use window
-        }
-      );
-      wow.init();
+        new WOW().init();
 
 
 });
